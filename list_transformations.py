faccaf9f1ca61b9061e934fdcf7052eeb01c5a5d
eqbrownISF <eqbrown@isf.edu.hk>
2020-11-25 09:07:52 +0800

    init commit

---
+++ list_transformations.py
@@ -0,0 +1,24 @@
+# Unit 0 Lab 2
+# Author: Your Name
+
+
+def reverse_list(a_list):
+    # YOUR CODE GOES HERE   
+
+
+
+def sort_list(a_list):
+    # YOUR CODE GOES HERE   
+
+
+
+def square_list(a_list):
+    # YOUR CODE GOES HERE   
+
+
+
+def capitalize_list(a_list):
+    # YOUR CODE GOES HERE   
+
+
+
