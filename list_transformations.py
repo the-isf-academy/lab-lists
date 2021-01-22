b8c8e3a4f05ca26532e9f6ec681f3179362a2676
eqbrownISF <eqbrown@isf.edu.hk>
2021-01-22 10:48:12 +0800

    updated files for student repos

--- list_transformations.py
+++ list_transformations.py
@@ -5,20 +5,22 @@
 def reverse_list(a_list):
     # YOUR CODE GOES HERE   
 
-
+    raise NotImplemented("This function isn't done!")
 
 def sort_list(a_list):
     # YOUR CODE GOES HERE   
 
-
+    raise NotImplemented("This function isn't done!")
 
 def square_list(a_list):
     # YOUR CODE GOES HERE   
 
+    raise NotImplemented("This function isn't done!")
 
 
 def capitalize_list(a_list):
     # YOUR CODE GOES HERE   
 
+    raise NotImplemented("This function isn't done!")
 
 
